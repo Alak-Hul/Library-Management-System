343374ef99f4b01be48c1d8dc732660a16a5d50c
Kackary <zackattackers@gmail.com>
2025-02-06 17:59:27 -0500

    Added new classes _LibraryItem, Magazine, and Account.
    Made Magazine and Book child classes of LibraryItem.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from Books import Book
 
-book1 = Book("The Witcher: Last Wish", "Angry Polish Guy")
+book1 = Book("The Witcher: Last Wish", "Angry Polish Guy","0-061-96436-0")
 
 print(f'{book1.is_status() = }')
+print(book1.ISBN,book1.author)
